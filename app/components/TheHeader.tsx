aa28b6c7c218cddd721eaa48494435606560b425
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-21 20:42:45 +0200

    feat: TEST

--- app/components/TheHeader.tsx
+++ app/components/TheHeader.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
 	container: {
 		paddingTop: 80,
 		paddingBottom: 30,
-		paddingHorizontal: 12,
+		paddingHorizontal: 13,
 		backgroundColor: "#0D60DD",
 		display: "flex",
 		flexDirection: "column",
